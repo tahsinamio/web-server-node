f07095d7b32647ed5f832848c1251a9f6c5304ab
Tahsin Wasit Amio <tahsinamio@gmail.com>
2018-04-28 13:18:51 +0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now  = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to log to the server');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('partials/maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'hello'
+  });
+})
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    welcomeMessage: 'About us'
+  });
+})
+
+app.get('/bad', (req, res) => {
+  res.send({
+    status: '404',
+    comment: 'Not found'
+  });
+})
+
+
+app.listen(3000, () => {
+  console.log('The server is up on port 3000');
+});
